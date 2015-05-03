b13cf82ff221a2767a97d23bbf4c36a9752c43b7
Roman Dubrovsky <roma2107946@gmail.com>
2015-05-03 15:12:15 +0300

    Init slim and cells

--- Gemfile
+++ Gemfile
@@ -12,6 +12,9 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem "slim-rails"
+gem 'cells', github: 'proghat/cells', branch: 'html_safe'
+
 group :development, :test do
   gem 'web-console', '~> 2.0'
   gem 'spring'
---
+++ app/concepts/application/cell.rb
@@ -0,0 +1,25 @@
+class Application::Cell < Cell::Concept
+  abstract!
+
+  include ApplicationHelper
+  include HtmlHelper
+  include ActionView::Helpers::FormHelper
+  include ActionView::Helpers::FormOptionsHelper
+  include ActionView::RecordIdentifier
+  #include SimpleForm::ActionViewExtensions::FormHelper
+  #include Ransack::Helpers::FormHelper
+  #include Devise::Controllers::Helpers
+
+  # In fact cells hardly depends on haml which defines Object#is_haml?
+  def is_haml?
+    false
+  end
+
+  private
+
+    def scoped_translation(key)
+      scope = self.class.name.sub(/::Cell$/, '').underscore.gsub('/', '.') + ".#{@view_path}"
+      I18n.t("#{scope}.#{key}")
+    end
+    alias :st :scoped_translation
+end
