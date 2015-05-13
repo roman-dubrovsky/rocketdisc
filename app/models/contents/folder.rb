419d2b32266d98a8193b7f195e2d48ce7c76dba1
Roman Dubrovsky <roma2107946@gmail.com>
2015-05-13 04:14:20 +0300

    Add destroy callback

--- app/models/contents/folder.rb
+++ app/models/contents/folder.rb
@@ -1,3 +1,11 @@
 class Contents::Folder < Content
   has_many :contents, class_name: 'Content', foreign_key: 'folder_id'
+
+  before_destroy :destroy_contents
+
+  private
+
+    def destroy_contents
+      contents.destroy_all
+    end
 end
